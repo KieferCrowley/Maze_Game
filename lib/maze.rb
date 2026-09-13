ed5816979bb2953aeecb23560772fd1bcac9893f
Samuel <jiaruifu@tamu.edu>
2026-09-12 23:20:43 -0500

    added 3 maze csv files and corresponding loading functions

--- lib/maze.rb
+++ lib/maze.rb
@@ -1,3 +1,5 @@
+require "csv"
+
 class Maze
     WALL = "###"
     EXIT = " X "
@@ -13,6 +15,7 @@ class Maze
     ["###", "###", "###", "###", "###", "###", "###"],
     ].freeze
 
+
     attr_reader :grid, :difficulty, :player_x, :player_y, :exit_x, :exit_y
 
     def initialize(difficulty)
@@ -20,24 +23,36 @@ class Maze
         
         case @difficulty
         when "easy"
-            @grid = TEST_GRID.map(&:dup) 
+            #easy grid size = 17*17
+            #@grid = TEST_GRID.map(&:dup) 
+            filename = "easy.csv"
             #.map(&:dup) creates line by line duplicate of the grid
             # this may come in handy when we make changes to it for movement or fog of war
         when "medium"
-            @grid = TEST_GRID.map(&:dup) 
+            #medium grid size = 23*23
+            #@grid = TEST_GRID.map(&:dup)
+            filename = "medium.csv"
         when "hard"
-            @grid = TEST_GRID.map(&:dup) 
+            #hard grid size = 29*29
+            #@grid = TEST_GRID.map(&:dup) 
+            filename = "hard.csv"
         else
             raise ArgumentError, "invalid difficulty: #{difficulty}, please type in easy, medium or hard"
         end
         #initiliazing important coordinates
         #start and exit located on top left and bottom right corner within the walls
+        path = File.expand_path("../mazes/#{filename}", __dir__)
+        @grid = CSV.read(path) 
+
         @player_x = 1
         @player_y = 1
         @exit_x = @grid[0].length - 2
         @exit_y = @grid.length- 2
     end
 
+    def load_maze(filename)
+        CSV.read(filename)
+    end
     # for quick look at whats in the current location
     def tile_at(x, y)
         @grid[y][x]
@@ -64,7 +79,7 @@ class Maze
 end 
 
 #test code
-#test_maze = Maze.new("easy")
+#test_maze = Maze.new("hard")
 #test_maze.test_display
 #testing valid move, expects false true true 
 #puts test_maze.valid_move?(0,0) 
