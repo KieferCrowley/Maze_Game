596a9601058a8a0de925d91fabd81d5f3c50b518
Samuel <jiaruifu@tamu.edu>
2026-09-13 16:44:08 -0500

    Added display function, completed game logic in main, known issue: display function's refresh clears invalid move notification from movement

--- maze_game.rb
+++ maze_game.rb
@@ -9,23 +9,27 @@ while (!input_valid) do
     difficulty = gets.downcase.strip
     if (difficulty == "easy" || difficulty == "medium" || difficulty == "hard")
         input_valid = true
-    else
-        puts "Invalid Input!"
     end
 end
 
 maze = Maze.new(difficulty)
 
-maze.test_display
+#maze.test_display
+maze.display
 
 game_over = false
-while (!game_over) do
+#temp step for test
+temp_step = 100
+while (!game_over && temp_step > 0) do
     action = STDIN.getch.downcase   # get single character input
     if (action == "w" || action == "a" || action == "s" || action == "d")
         maze.move(action) # move() checks if move is valid
+        maze.display
         if (maze.player_at_exit?)
             game_over = true
+            puts "You Escaped!"
         end
     end
-    game_over = true # temp exit. delete once player position updates are displayed
+    temp_step -= 1
+    #game_over = true # temp exit. delete once player position updates are displayed
 end
