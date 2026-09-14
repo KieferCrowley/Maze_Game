5373dade4860e1a60ef36e9d64f5b6668e8d1a24
KieferCrowley <kiefercrowley@tamu.edu>
2026-09-14 17:27:36 -0500

    Implemented timer
    Timer counts down from 60. Game (sort of) ends when timer reaches 0. Doesn't actually end until next key press.

---
+++ lib/timer.rb
@@ -0,0 +1,18 @@
+class Timer
+    attr_reader :time
+
+    def initialize(time)
+        @time = time
+    end
+
+    def count_down()
+        # create thread so timer can run in parallel with game.
+        Thread.new do
+            while (@time >= 0) do
+                print "\rTime Remaining: #{@time}"
+                @time -= 1
+                sleep(1)
+            end
+        end
+    end
+end
--- maze_game.rb
+++ maze_game.rb
@@ -1,6 +1,7 @@
 require 'io/console'
 
 require_relative 'lib/maze'
+require_relative 'lib/timer'
 
 # Prompt for user input until valid input is received
 input_valid = false
@@ -14,22 +15,21 @@ end
 
 maze = Maze.new(difficulty)
 
-#maze.test_display
 maze.display
 
+timer = Timer.new(60)
+timer.count_down
+
+# Start game. Game ends when player reaches the end or time runs out.
 game_over = false
-#temp step for test
-temp_step = 100
-while (!game_over && temp_step > 0) do
-    action = STDIN.getch.downcase   # get single character input
+while (!game_over && timer.time > 0) do
+    action = STDIN.getch.downcase   # get single character input. BUG: blocking action
     if (action == "w" || action == "a" || action == "s" || action == "d")
         maze.move(action) # move() checks if move is valid
-        maze.display
+        maze.display # update view of maze
         if (maze.player_at_exit?)
             game_over = true
             puts "You Escaped!"
         end
     end
-    temp_step -= 1
-    #game_over = true # temp exit. delete once player position updates are displayed
 end
